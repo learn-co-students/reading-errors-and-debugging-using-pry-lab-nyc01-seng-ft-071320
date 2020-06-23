27ad2cf2fa3b2a6a4c7cf83869c3feb3f4444d5a
David Kirsch <davidjkirsch@gmail.com>
2020-06-23 01:01:26 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,13 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    new_string = ""
+    10.times do
+    new_string += "s"
+  end
+  new_string + string
   else
     string
   end
