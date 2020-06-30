70356b8039dd876ebb6c442e5096976b9a24b6e4
Jzavier Timm <jztimm@gmail.com>
2020-06-30 01:14:08 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,16 @@
 # don't forget to add: require 'pry'
 
+require 'pry'
+
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    # long_s = 10.times { print "s" }       # Not sure why this doesn't work
+    long_s = "s" * 10
+    
   else
     string
   end
+  new_str = long_s.to_s + string
+  puts new_str
+  new_str
 end
