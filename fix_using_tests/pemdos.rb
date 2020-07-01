54fb8149c94b89071166195aeeccd340885aa245
Mimi Ojserkis <mimioj@gmail.com>
2020-07-01 20:34:04 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,20 @@
 # don't forget to add: require 'pry'
 
-def snake_it_up(string)
-  if string[0] == "s"
-    10 * "s" + string
-  else
-    string
-  end
-end
+# def snake_it_up(string)
+#   if string[0] == "s"
+#     10 * "s" + string
+#   else
+#     string
+#   end
+# end
+
+
+ def snake_it_up(string)
+   if string.start_with? 's'
+     "ssssssssss" + string
+   else
+     string
+   end
+ end
+
+snake_it_up("surprise!")
