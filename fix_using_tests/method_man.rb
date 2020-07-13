bc3db4621f8b9296fea1c4fbc398aa6fc0ba940b
github username <joshmrallen@gmail.com>
2020-07-13 12:27:45 -0400

    Done.

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,10 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def start_game(player1, player2)
-  "Hello #{player1} & #{playar2}"
+  "Hello #{player1} & #{player2}" #there was a typo in the variable name player2
 end
 
-def play_game(player2)
+def play_game(player1, player2) #method only had one argument
   "#{player1} is better than #{player2}"
 end
--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,9 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    "s"*10 + string #to make multiple of a string, you need to put string * integer, not the other way around or else it'll think you're trying to multiple string as an integer
   else
     string
   end
